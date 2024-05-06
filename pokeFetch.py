eba056e07bd56915749a9f238acd002a0a45063e
matterofact <mattosborn85@protonmail.com>
2024-05-06 11:45:26 +0100

    Creates initial widget

--- pokeFetch.py
+++ pokeFetch.py
@@ -1,34 +1,47 @@
 
 import requests
-import cs50
-import sys
-import random
-import PIL.ImageQt, PIL.Image
 import urllib3
+import pypokedex
+import django
+import pokepy
+import kivy.graphics
+import sys
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.image import AsyncImage
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
 from io import BytesIO
 
 
 def main():
-
     if __name__ == '__main__':
         pokeFetchApp().run()
 
-class pokeFetchfunc(Widget):
-    pass
+    
+class pokeFetch(Widget):
+    search = 'bulbasaur'
+    pokemon = pypokedex.get(name=str(search))
+    dexId = str(pokemon.dex)
+    img = pokemon.sprites.front['default']
+    name = pokemon.name.title()
+    if len(pokemon.types) > 1:
+        types = str(pokemon.types[0]).title() + ', ' + str(pokemon.types[1]).title()
+    else:
+        types = str(pokemon.types[0]).title()
+
+    def on_enter(self):
+        search = self.ids.input.text
+        return pokeFetch()
+
 
 class pokeFetchApp(App):
     def build(self):
-        return pokeFetchfunc()
-
-    #http = urllib3.PoolManager()
-    #response = requests.get(bulba)
-    #image = PIL.Image.open(BytesIO(response))
-    #img = PIL.Image.Image(image)
-
-
+        return pokeFetch()
 
 
 
