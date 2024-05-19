b87dc60eee66f149793a2f88dc98dfc7827e4d56
matterofact <mattosborn85@protonmail.com>
2024-05-19 12:14:38 +0100

    Added parties page, allows user to add parties to the database and then list the contents of each party. Parties are just numbered, but they could be named to make it easier to keep track of each party. Fixed font not rendering correctly on summary pokedex page

--- pokeFetch.py
+++ pokeFetch.py
@@ -38,14 +38,50 @@ def clear_window(root, frm, search, user_id):
 
 def createParty(root, frm, user_id):
     # TODO Need to allow for looping submission of pokemon into a party, and then insert into the parties database
-    for i in range(1,7):
-        party_member_label  = tk.Label(frm, text="Pokemon " + str(i) + ":")
-        party_member_label.pack()
-        party_member_entry = ttk.Entry(frm, font=('',15))
-        party_member_entry.pack()
-    submit_party_button = ttk.Button(text='Submit Party')
 
+    # Clear the frame
+    for widget in frm.winfo_children():
+        widget.destroy()
 
+    # List to store entry widgets
+    entry_widgets = []
+
+    # Create entry widgets for Pokémon party members
+    for i in range(1, 7):
+        party_member_label = tk.Label(frm, text="Pokemon " + str(i) + ":", font=('', 15))
+        party_member_label.pack(pady=5)
+
+        party_member_entry = ttk.Entry(frm, font=('', 15))
+        party_member_entry.pack(pady=5)
+
+        # Add the entry widget to the list
+        entry_widgets.append(party_member_entry)
+
+    # Submit button
+    submit_party_button = ttk.Button(frm, text='Submit Party', command=lambda: submitParty(entry_widgets, user_id))
+    submit_party_button.pack(pady=10)
+
+def submitParty(entry_widgets, user_id):
+    # Collect the values from the entry widgets
+    party_data = [entry.get() for entry in entry_widgets]
+
+    # Print collected data for debugging (you can replace this with actual database insertion code)
+    print("Collected Party Data:")
+    for i, pokemon in enumerate(party_data, start=1):
+        print(f"Pokemon {i}: {pokemon}")
+    
+    # Insert the data into the database
+    insert_party_into_db(user_id, party_data)
+
+def insert_party_into_db(user_id, party_data):
+    c.execute('''
+        INSERT INTO parties (user_id, pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6)
+        VALUES (?, ? ,?, ?, ?, ?, ?)
+    ''', (user_id, *party_data))
+    db.commit()
+
+    partiesWindow(root, frm, user_id)
+    
 def hash_password(plain_text_password):
     return bcrypt.hashpw(plain_text_password, bcrypt.gensalt())
 
@@ -94,7 +130,7 @@ def loginWindow(frm):
     password_entry.pack()
 
     loginButton = ttk.Button(frm, text="Login", command=lambda: login(username_entry.get(), password_entry.get(), root, frm))
-    loginButton.pack()
+    loginButton.pack(pady=20)
 
     registerButton = ttk.Button(frm, text="Register", command=lambda: registerWindow(root, frm))
     registerButton.pack()
@@ -105,13 +141,57 @@ def partiesWindow(root, frm, user_id):
     frm.destroy()
     frm = ttk.Frame(root, padding=10)
     frm.pack()
-    c.execute(
+
+    parties = c.execute(
             "SELECT * FROM parties WHERE user_id = ?", [user_id]
         )
-    new_party_button = ttk.Button(frm, text="New party", command=lambda: createParty(root, frm, user_id))
-    new_party_button.pack() 
+    parties = c.fetchall()
 
-    pass
+    # Remove the first two elements from each tuple
+    for i in range(0,len(parties)):
+        party_contents = [party[2:] for party in parties]
+        party = ttk.Label(frm, text='Party ' + str(i+1))
+        party.pack()
+        # Button to view detailed summary of the first party for demonstration
+        summary_button = ttk.Button(frm, text='View Party Summary', command=lambda i=i: partySummaryWindow(root, frm, user_id, parties[i][2:]))
+        summary_button.pack(pady=10)
+
+    new_party_button = ttk.Button(frm, text="New party", command=lambda: createParty(root, frm, user_id))
+    new_party_button.pack()
+
+def partySummaryWindow(root, frm, user_id, party):
+    # Clear the frame
+    for widget in frm.winfo_children():
+        widget.destroy()
+
+    root.title("Party Summary")
+
+    # Create a new frame for the party summary
+    summary_frame = ttk.Frame(frm, padding=10)
+    summary_frame.grid()
+
+    # Fetch and display each Pokémon's sprite and name
+    for i, pokemon_name in enumerate(party):
+        if pokemon_name:
+            pokemon = pypokedex.get(name=str(pokemon_name).lower())
+            poke_name = pokemon.name.title()
+            sprite_url = pokemon.sprites.front['default']
+            response = requests.get(sprite_url)
+            sprite_image = PIL.Image.open(BytesIO(response.content))
+            sprite_photo = ImageTk.PhotoImage(sprite_image)
+
+            name_label = ttk.Label(summary_frame, text=poke_name, font=('', 15))
+            name_label.grid(column=0, row=i, padx=5, pady=5, sticky='w')
+
+            # Display the sprite and name
+            sprite_label = ttk.Label(summary_frame, image=sprite_photo)
+            sprite_label.image = sprite_photo  # Keep a reference to avoid garbage collection
+            sprite_label.grid(column=1, row=i, padx=5, pady=5)
+
+    # Back button to go back to the parties window
+    back_button = ttk.Button(summary_frame, text='Back', command=lambda: partiesWindow(root, frm, user_id))
+    back_button.grid(column=0, row=len(party) + 1, columnspan=2, pady=10)
+ 
 
 def registerWindow(root, frm):
 
@@ -170,13 +250,13 @@ def register(root, frm, username, password, passwordConfirmation):
 def summaryWindow(search, root, frm, user_id):
     http = urllib3.PoolManager()
     root.title("Summary: " + search.title())
-    frm = ttk.Frame(padding=10)
+    frm = ttk.Frame(root, padding=10)
     frm.grid()
-    frm.grid_columnconfigure(16, minsize=100)
-    frm.grid_rowconfigure(16, minsize=100)
+    frm.grid_columnconfigure(0, weight=1)
+    frm.grid_rowconfigure(0, weight=1)
     frm.focus()
 
-    pokemon = pypokedex.get(name=search)       
+    pokemon = pypokedex.get(name=search)
     dexId = str(pokemon.dex)
     pokeName = pokemon.name.title()
     url = pokemon.sprites.front['default']
@@ -185,55 +265,49 @@ def summaryWindow(search, root, frm, user_id):
     temp = PIL.Image.open(BytesIO(response.content))
     sprite = ImageTk.PhotoImage(temp)
 
-    if len(pokemon.types) > 1:
-        types = str(pokemon.types[0]).title() + ', ' + str(pokemon.types[1]).title()
-    else:
-        types = str(pokemon.types[0]).title()
-    
-    if len(pokemon.abilities) > 1:
-        abilities = str(pokemon.abilities[0][0]).title() + ', ' + str(pokemon.abilities[1][0]).title()
-    else:
-        abilities = str(pokemon.abilities[0][0]).title()
+    types = ', '.join([t.title() for t in pokemon.types])
+    abilities = ', '.join([a[0].title() for a in pokemon.abilities])
 
-    # TODO Abilities don't render text properly
-    spriteImg = Label(frm, image=sprite).grid(column=0, row=0)
-    idLabel = Label(frm, text="Pokedex ID: ", font=('',15)).grid(column=0, row=1, sticky='w')
-    idValue = Label(frm, text=dexId, font=('',15)).grid(column=1, row=1, sticky='w') 
-    nameLabel = Label(frm, text="Name: ", font=('',15)).grid(column=0, row=2, sticky='w')
-    nameValue = Label(frm, text=pokeName, font=('',15)).grid(column=1, row=2, sticky='w')
-    typeLabel = Label(frm, text="Type: ", font=('', 15)).grid(column=0, row=3, sticky='w')
-    typeValue = Label(frm, text=types, font=('',15)).grid(column=1, row=3, sticky='w')
-    abilitiesLabel = Label(frm, text="Ability: ", font=('', 15)).grid(column=0, row=4, sticky='w')
-    abilitiesValue = Label(frm, text=abilities, font=('', 15)).grid(column=1, row=4)
-    Label(frm, text='', font=('',15)).grid(column=1, row=4, sticky='w')
-    statsLabel = Label(frm, text="Stats: ", font=('',15)).grid(column=0, row=5, sticky='w')
-    Label(frm, text='', font=('',15)).grid(column=1, row=6, sticky='w')
-    hpLabel = Label(frm, text="HP: ", font=('', 15)).grid(column=0, row=7, sticky='w')
-    hpValue = Label(frm, text=stats[0], font=('',15)).grid(column=1, row=7, sticky='w')
-    atkLabel = Label(frm, text="Attack: ", font=('', 15)).grid(column=0, row=8, sticky='w')
-    atkValue = Label(frm, text=stats[1], font=('',15)).grid(column=1, row=8, sticky='w')
-    defLabel = Label(frm, text="Defence: ", font=('', 15)).grid(column=0, row=9, sticky='w')
-    defValue = Label(frm, text=stats[2], font=('',15)).grid(column=1, row=9, sticky='w')
-    sp_atkLabel = Label(frm, text="Sp Atk: ", font=('', 15)).grid(column=0, row=10, sticky='w')
-    sp_atkValue = Label(frm, text=stats[3], font=('',15)).grid(column=1, row=10, sticky='w')
-    sp_defLabel = Label(frm, text="Sp Def: ", font=('', 15)).grid(column=0, row=11, sticky='w')
-    sp_defValue = Label(frm, text=stats[4], font=('',15)).grid(column=1, row=11, sticky='w')
-    speedLabel = Label(frm, text="Speed: ", font=('', 15)).grid(column=0, row=12, sticky='w')
-    speedValue = Label(frm, text=stats[5], font=('',15)).grid(column=1, row=12, sticky='w')
-    Label(frm, text='', font=('',15)).grid(column=1, row=13, sticky='w')
+    # Creating widgets
+    labels = [
+        ("Pokedex ID: ", dexId),
+        ("Name: ", pokeName),
+        ("Type: ", types),
+        ("Ability: ", abilities),
+        ("Stats: ", ""),
+        ("HP: ", stats[0]),
+        ("Attack: ", stats[1]),
+        ("Defense: ", stats[2]),
+        ("Sp Atk: ", stats[3]),
+        ("Sp Def: ", stats[4]),
+        ("Speed: ", stats[5]),
+    ]
 
-    # TODO Add ability to press enter to hit search and login buttons
-    search_label = ttk.Label(frm, text="Search for a Pokemon: ", font=('',15)).grid(column=0, row=14, sticky='w')
+    spriteImg = tk.Label(frm, image=sprite)
+    spriteImg.grid(column=0, row=0, columnspan=2, pady=10)
+
+    for i, (label_text, value_text) in enumerate(labels, start=1):
+        tk.Label(frm, text=label_text, font=('', 15)).grid(column=0, row=i, sticky='w', padx=5)
+        tk.Label(frm, text=value_text, font=('', 15)).grid(column=1, row=i, sticky='w', padx=5)
+
+    # Adding a blank label to create a gap
+    blankLabel = tk.Label(frm, text="")
+    blankLabel.grid(column=0, row=len(labels) + 1, pady=10)
+
+    search_label = ttk.Label(frm, text="Search for a Pokemon: ", font=('', 15))
     pokeSearch = tk.StringVar()
-    searchBox = ttk.Entry(frm, textvariable=pokeSearch, font=('',15))
-    searchBox.grid(column=0, row=15)
+    searchBox = ttk.Entry(frm, textvariable=pokeSearch, font=('', 15))
     searchButton = ttk.Button(frm, text="Search", command=lambda: clear_window(root, frm, pokeSearch.get(), user_id))
-    searchButton.grid(column=2, row=15, sticky='w')
     partiesButton = ttk.Button(frm, text="Parties", command=lambda: partiesWindow(root, frm, user_id))
-    partiesButton.grid(column=0, row=16, sticky='w')
+
+    search_label.grid(column=0, row=len(labels) + 2, sticky='w', padx=5)
+    searchBox.grid(column=1, row=len(labels) + 2, sticky='w', padx=5)
+    searchButton.grid(column=2, row=len(labels) + 2, sticky='w', padx=5)
+    partiesButton.grid(column=0, row=len(labels) + 3, sticky='w', pady=10)
+
     root.mainloop()
 
 root = Tk()
-root.geometry("500x750")
+root.geometry("525x750")
 frm = ttk.Frame(root, padding=10)
 loginWindow(frm)
